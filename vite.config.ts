6cffe6a1c8e7771c7d2cf13cf069507dcd6fe9b5
Fabian Huhn <fabian@einzelmensch.de>
2026-07-26 19:49:21 +0200

    switch to netlify adapter

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-node";
+import adapter from "@sveltejs/adapter-netlify";
 import { defineConfig, lazyPlugins } from "vite-plus";
 
 export default defineConfig({
